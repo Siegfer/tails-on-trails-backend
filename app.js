20cca98348aa8062bc5dcf97ad7aae7b7db1acc5
siegfer <nicksalguerobui@icloud.com>
2021-09-27 22:13:46 -0700

    Update: App.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const passport = require('passport')
+require('./config/passport')(passport)
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
+app.use(passport.initialize())
 
 // import models
 // app.use('/shelters', require('./controllers/shelters'))
