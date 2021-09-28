744edcbcb012405eee26078510e0394e4d30c834
siegfer <nicksalguerobui@icloud.com>
2021-09-28 14:16:03 -0700

    dependencies changes

--- controllers/shelters.js
+++ controllers/shelters.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const router = express()
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = process.env
 const passport = require('passport')
